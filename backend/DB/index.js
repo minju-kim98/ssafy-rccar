4de7d1a6ae739a0bfc69b91d0f08b9c0149de1e7
minju-kim98 <ducco705@snu.ac.kr>
2023-10-31 00:16:01 +0900

    v0.0

---
+++ backend/DB/index.js
@@ -0,0 +1,15 @@
+// get the client
+const mysql = require('mysql2/promise');
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: '3.36.75.253',
+  user: 'mj3meal',
+  password: "981107",
+  database: 'RCcarDB',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool
